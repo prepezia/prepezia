74e56de53b46830d1634da5c0403063683efd79c
Kofi Oppong <learnwithtemi.ai@gmail.com>
2026-01-21 11:37:28 +0000

    lets remove this (_for element p_)

--- src/components/layout/HomeHeader.tsx
+++ src/components/layout/HomeHeader.tsx
@@ -4,10 +4,7 @@ import { UserNav } from "@/components/layout/UserNav";
 
 export function HomeHeader() {
     return (
-        <header className="flex items-center justify-between p-4">
-            <div className="flex items-center gap-4">
-                <p className="font-semibold text-lg">Welcome back, Username!</p>
-            </div>
+        <header className="flex items-center justify-end p-4">
             <UserNav />
         </header>
     )
