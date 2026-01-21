a1265d171d1ec151447d415988af6fa1822f770c
Kofi Oppong <learnwithtemi.ai@gmail.com>
2026-01-21 11:07:05 +0000

    lets change this to something simple showing a head drawing  (_for eleme

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 export const Logo = ({ className }: { className?: string }) => (
   <svg
     className={className}
-    viewBox="0 0 100 100"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="currentColor"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1.5"
+    strokeLinecap="round"
+    strokeLinejoin="round"
   >
-    <path d="M50,10 C27.9,10 10,27.9 10,50 C10,72.1 27.9,90 50,90 C72.1,90 90,72.1 90,50 C90,27.9 72.1,10 50,10 Z M50,85 C30.7,85 15,69.3 15,50 C15,30.7 30.7,15 50,15 C69.3,15 85,30.7 85,50 C85,69.3 69.3,85 50,85 Z" />
-    <path d="M42,30 H58 V45 H65 V30 H70 V70 H65 V50 H42 V70 H37 V30 H42 V45 H42z" transform="translate(-3, -2)" />
+    <path d="M5.52 19c.64-2.2 1.84-3 3.22-3h6.52c1.38 0 2.58.8 3.22 3" />
+    <circle cx="12" cy="10" r="3" />
+    <circle cx="12" cy="12" r="10" />
   </svg>
 );
