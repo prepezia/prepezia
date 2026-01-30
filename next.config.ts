e24beb0eb705788d48a0b0e8a4ae27fcc431e272
Kofi Oppong <learnwithtemi.ai@gmail.com>
2026-01-30 11:23:50 +0000

    This error occurs because Next.js has a default security limit of **1 MB

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverActions: {
-    bodySizeLimit: '50mb',
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb',
+    },
   },
   typescript: {
     ignoreBuildErrors: true,
