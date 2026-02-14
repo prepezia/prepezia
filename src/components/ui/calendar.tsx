356a9ff31cf50dabccd213396b91e59aebe31891
Kofi Oppong <learnwithtemi.ai@gmail.com>
2026-02-14 14:04:41 +0000

    tsc --noEmit --pretty src/app/home/study-spaces/page.tsx:1127:18 - error

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -56,7 +56,6 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
         IconRight: () => <ChevronRight className="h-4 w-4" />,
         ...props.components,
       }}
