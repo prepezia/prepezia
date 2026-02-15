79a84ec173b01a56c2fc297ac5b378f022274670
Kofi Oppong <learnwithtemi.ai@gmail.com>
2026-02-15 22:20:21 +0000

    Are we not using the wrong storage bucket address - see screenshot (i am

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyB_jUNf5ENWbTmK5zJm7UsO0QfgMrQM1A4",
   authDomain: "studio-4412321193-4bb31.firebaseapp.com",
   projectId: "studio-4412321193-4bb31",
-  storageBucket: "studio-4412321193-4bb31.appspot.com",
+  storageBucket: "studio-4412321193-4bb31.firebasestorage.app",
   messagingSenderId: "386931701755",
   appId: "1:386931701755:web:3cc26941d826b5d1e0fd5e",
 };
