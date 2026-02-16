d20bbaf979eeaaca083819cf27d54847e5500623
Kofi Oppong <learnwithtemi.ai@gmail.com>
2026-02-16 01:03:02 +0000

    the logo image is not very sharp - lets try this high res image - https:

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -5,7 +5,7 @@ export const Logo = ({ className }: { className?: string }) => {
     return (
         <div className={`relative ${className || 'w-8 h-8'}`}>
             <Image
-                src="https://firebasestorage.googleapis.com/v0/b/studio-4412321193-4bb31.firebasestorage.app/o/public%2Fprepezia%20logo%20-%202).png?alt=media&token=0fb17391-9e36-409f-a1da-16c2d266d74c"
+                src="https://firebasestorage.googleapis.com/v0/b/studio-4412321193-4bb31.firebasestorage.app/o/public%2Fprepezia%20logo%20-%20Copy.png?alt=media&token=3d0f28b4-8873-4b6f-aa46-e6a4909d6448"
                 alt="Prepezia Logo"
                 fill
                 className="object-contain"
