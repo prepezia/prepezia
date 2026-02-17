622494689b8995a4c1596154bdd0cab35209d0f3
Kofi Oppong <learnwithtemi.ai@gmail.com>
2026-02-17 04:26:21 +0000

    how do we implement this  - Progressive Web App  /// should we implement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head />
+      <head>
+          <meta name="application-name" content="Prepezia" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content="Prepezia" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#5f43b6" />
+          <link rel="apple-touch-icon" href="https://firebasestorage.googleapis.com/v0/b/studio-4412321193-4bb31.firebasestorage.app/o/public%2Fprepezia%20logo%20-%20Copy.png?alt=media&token=3d0f28b4-8873-4b6f-aa46-e6a4909d6448" />
+          <link rel="manifest" href="/manifest.json" />
+      </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>
           {children}
