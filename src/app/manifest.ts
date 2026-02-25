49a262227d5c35539903e9e6937cc0b6ebaba913
Kofi Oppong <learnwithtemi.ai@gmail.com>
2026-02-25 03:49:25 +0000

    A. i have sorted the favicon issue - i had to delete the image and reupl

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
+  const logoUrl = 'https://firebasestorage.googleapis.com/v0/b/studio-4412321193-4bb31.firebasestorage.app/o/public%2Fprepezia%20logo%20-%20Copy.png?alt=media&token=3d0f28b4-8873-4b6f-aa46-e6a4909d6448';
+  
   return {
     name: 'Prepezia',
     short_name: 'Prepezia',
@@ -11,25 +13,25 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#5f43b6',
     icons: [
       {
-        src: 'https://firebasestorage.googleapis.com/v0/b/studio-4412321193-4bb31.firebasestorage.app/o/public%2Ffavicon.png?alt=media&token=2eba5a56-46dc-44db-9d8c-a91cff1f4f2c',
+        src: logoUrl,
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: 'https://firebasestorage.googleapis.com/v0/b/studio-4412321193-4bb31.firebasestorage.app/o/public%2Ffavicon.png?alt=media&token=2eba5a56-46dc-44db-9d8c-a91cff1f4f2c',
+        src: logoUrl,
         sizes: '192x192',
         type: 'image/png',
         purpose: 'any',
       },
       {
-        src: 'https://firebasestorage.googleapis.com/v0/b/studio-4412321193-4bb31.firebasestorage.app/o/public%2Ffavicon.png?alt=media&token=2eba5a56-46dc-44db-9d8c-a91cff1f4f2c',
+        src: logoUrl,
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: 'https://firebasestorage.googleapis.com/v0/b/studio-4412321193-4bb31.firebasestorage.app/o/public%2Ffavicon.png?alt=media&token=2eba5a56-46dc-44db-9d8c-a91cff1f4f2c',
+        src: logoUrl,
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any',
